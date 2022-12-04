6fc754fb70f7f3e75e0bee5c8242f4307c9b25a3
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-12-04 11:55:46 -0500

    commit tree 849c8949aa0d25bdb451bd45173866aa968443ad

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  # session[:page_views] ||= 3
+
+
   def index
+    session[:page_views] ||= 0
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    
     article = Article.find(params[:id])
     render json: article
   end
